7447af7bd0578b88e1953f23496105bea1ec72d3
BoonDoPark <76871728+BoonDoPark@users.noreply.github.com>
2024-03-30 16:34:33 +0900

    elasticsearch 설치

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ElasticsearchModule } from './elasticsearch/elasticsearch.module';
+import { SearchModule } from './search/search.module';
 import { UserModule } from './user/user.module';
 import { PlaylistService } from './playlist/playlist.service';
 import { PlaylistModule } from './playlist/playlist.module';
@@ -7,7 +7,7 @@ import { MusicController } from './music/music.controller';
 import { MusicModule } from './music/music.module';
 
 @Module({
-  imports: [ElasticsearchModule, UserModule, PlaylistModule, MusicModule],
+  imports: [SearchModule, UserModule, PlaylistModule, MusicModule],
   controllers: [MusicController],
   providers: [PlaylistService],
 })
--- server/src/elasticsearch/elasticsearch.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('elasticsearch')
-export class ElasticsearchController {}
