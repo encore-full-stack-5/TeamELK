d67697ea5766c978d57e7f9d215832f746237d7d
143xx <71882579+m48483@users.noreply.github.com>
2024-04-04 10:10:10 +0900

    recoil, axios, router, tailwind 추가

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Counter />,
+  },
+  {
+    path: "/join",
+    element: <Signup />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RecoilRoot>
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
+  </RecoilRoot>
 )
