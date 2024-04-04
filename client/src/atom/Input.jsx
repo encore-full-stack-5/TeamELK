bdd5ea61f75855d2edca13e83898043b173c81c8
143xx <71882579+m48483@users.noreply.github.com>
2024-04-04 14:20:05 +0900

    Signup

---
+++ client/src/atom/Input.jsx
@@ -0,0 +1,11 @@
+const Input = (props) => {
+  return (
+    <input
+      {...props}
+      className="m-2 peer h-full w-full rounded-md border border-blue-gray-200 bg-transparent px-3 py-3 font-sans text-m font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-green-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+      style={{ color: "black" }} // 텍스트 색상을 검은색으로 지정
+    />
+  );
+};
+
+export default Input;
--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // CORS 설정
+  app.enableCors({
+    origin: 'http://localhost:5173', // 허용할 오리진
+    methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE', // 허용할 HTTP 메서드
+    allowedHeaders: 'Content-Type', // 허용할 요청 헤더
+  });
   await app.listen(3000);
 }
 bootstrap();
