27a207b025867da6c1ba728475b140f941cf03bc
Kim JaeMin <2098kjm@naver.com>
2024-04-05 15:33:53 +0900

    header fixed

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,11 +1,11 @@
 // pages/Layout.jsx
 import { Outlet } from "react-router-dom";
-import NavBar from "../components/NavBar";
+import MavBar from "../components/NavBar";
 const Layout = () => {
   return (
     <div>
-      <header className="mb-4 pb-3">
-        <NavBar />
+      <header className="header mb-4 pb-3">
+        <MavBar />
       </header>
       <main>
         <Outlet />
