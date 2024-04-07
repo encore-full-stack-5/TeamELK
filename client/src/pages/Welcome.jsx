fe75a9d08a364537946a3237109953a361bdecb6
kkshyun <kkshyun@gmail.com>
2024-04-07 18:42:04 +0900

    logout page 완성

--- client/src/pages/Welcome.jsx
+++ client/src/pages/Welcome.jsx
@@ -1,11 +1,22 @@
 import Button from "../atom/Button";
-
+import { useNavigate } from "react-router-dom";
 const Welcome = () => {
+  const navigate = useNavigate();
   //   console.log("welcome 들어옴");
+  const moveToPlaylist = () => {
+    navigate("/playlist");
+  };
 
   return (
     <>
-      <Button>{`${localStorage.getItem("nickName")}님 환영합니다!`}</Button>
+      <button
+        onClick={moveToPlaylist}
+        class="transition ease-in-out delay-150 bg-green-500 text-white shadow-md shadow-green-500/20 hover:shadow-lg hover:shadow-green-500/40 font-bold py-3 px-8 rounded-full text-center hover:-translate-y-1 hover:scale-110  duration-300 "
+      >
+        {localStorage.getItem("nickName") !== null
+          ? `${localStorage.getItem("nickName")}님 환영합니다!`
+          : "환영합니다!"}
+      </button>
     </>
   );
 };
