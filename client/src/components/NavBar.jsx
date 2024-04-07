a1d42e004bb5a183bad673d8b38d7f0ec332a14f
kkshyun <kkshyun@gmail.com>
2024-04-07 14:54:32 +0900

    logout try

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 //  components/MavBar.jsx
 const MavBar = () => {
+  const [msg, setMsg] = useState("login");
+
+  // 컴포넌트 내부의 상태 변경 로직
+  useEffect(() => {
+    if (localStorage.getItem("uid") !== null) {
+      setMsg("logout");
+    }
+  }, []);
+  const localStorageClear = () => {
+    if (window.confirm("로그아웃하시겠습니까?")) {
+      localStorage.clear();
+      alert("로그아웃");
+      console.log(localStorage.getItem("uid"));
+    }
+    localStorage.clear();
+  };
   return (
     <>
       <nav className="flex justify-between">
@@ -16,9 +33,15 @@ const MavBar = () => {
             내 플레이리스트
           </Link>
         </div>
-        <Link to="/login" className="nav-login">
-          login
-        </Link>
+        {msg === "logout" ? (
+          <Link to="/" className="nav-login" onClick={localStorageClear}>
+            logout
+          </Link>
+        ) : (
+          <Link to="/login" className="nav-login">
+            login
+          </Link>
+        )}
       </nav>
 
       <hr className="home-hr" />
